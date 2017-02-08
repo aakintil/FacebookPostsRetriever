1d42f07dec445f28e037bc0d7c8d299381f3e7f7
Aderinsola Akintilo <aderinsola419@gmail.com>
2017-02-08 09:42:03 -0800

    creating custom facebook request module

---
+++ custom_modules/facebook-posts-request.js
@@ -0,0 +1,18 @@
+//// https://github.com/request/request
+//var request = require('request');
+//// =============================================================================
+//// USE REQUEST TO GET THE DATA
+//// =============================================================================
+//request('http://www.aderinsola.com', function (error, response, body) {
+//  if (!error && response.statusCode == 200) {
+//    console.log("request is running") // Show the HTML for the Google homepage.
+//    console.log(body) // Show the HTML for the Google homepage.
+//  }
+//})
+var exports = module.exports = {};
+exports.sayHelloInEnglish = function () {
+  return "HELLO";
+};
+exports.sayHelloInSpanish = function () {
+  return "Hola";
+};
